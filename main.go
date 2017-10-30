ae097fff26412cdf535894cd3f91f58ef02cb9f8
TTS기술담당 John berrybryan <skt.1902654@sk.com>
2017-10-30 12:02:31 +0900

    Retreiving count players works. Need to decide what's next

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"encoding/json"
 	"os"
 	"flag"
+	"net/http"
+	"fmt"
+	"strconv"
+	"io/ioutil"
 )
 
 type Config struct {
@@ -13,15 +17,63 @@ type Config struct {
 	PubgAppId int
 }
 
+type PlayerCountResponse struct {
+	Response PlayerCountResponseData `json:"response"`
+}
+
+type PlayerCountResponseData struct {
+	PlayerCount int `json:"player_count"`
+	Result int `json:"result"`
+}
+
 func main() {
 	var configFilePath string
 	flag.StringVar(&configFilePath, "config_file_path", "config.json", "Path to the JSON configuration file. See default.json.template for an example")
 	flag.Parse()
 
 	config := ReadConfigs(configFilePath)
-	log.Print(config)
 
+	playerCountData := GetPlayerCountData(config)
+	if playerCountData == nil {
+		log.Fatal("Failed to collect player count data.")
+	}
+	log.Printf("Count players: %d -- Result: %d", playerCountData.Response.PlayerCount, playerCountData.Response.Result)
+}
 
+func GetPlayerCountData(config *Config) (*PlayerCountResponse) {
+
+	request, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%sISteamUserStats/GetNumberOfCurrentPlayers/v1/", config.ApiBaseUrl), nil)
+	if err != nil {
+		log.Printf("Error creating request for player count data. Error was: %s", err)
+		return nil
+	}
+	queryParams := request.URL.Query()
+	queryParams.Add("appid", strconv.Itoa(config.PubgAppId))
+	request.URL.RawQuery = queryParams.Encode()
+	log.Print(request.URL.String())
+
+	headers := request.Header
+	headers.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Printf("Error in response when collecting player count data. Error was: %s", err)
+		return nil
+	}
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Error reading response body when collecting player count data. Error was: %s", err)
+		return nil
+	}
+	var playerCountData = new(PlayerCountResponse)
+	err = json.Unmarshal(responseBody, &playerCountData)
+	if err != nil {
+		log.Printf("Error decoding response body for player count data. Error was: %s", err)
+		log.Printf("Response body was: %s", responseBody)
+		return nil
+	}
+	return playerCountData
 }
 
 // https://stackoverflow.com/a/16466189/795407
@@ -29,7 +81,6 @@ func ReadConfigs(fileName string) *Config {
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalf("Error reading in file '%s'. Error was: %s", fileName, err)
-		os.Exit(-1)
 	}
 
 	decoder := json.NewDecoder(file)
